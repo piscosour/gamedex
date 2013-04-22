79aa4e3b99b5b3271a42ffc8d92cbc8ba1d26e39
eduardo <piscosour@gmail.com>
2013-04-22 01:39:56 -0400

    Updated operations file - games_per_year (lacking sort)

--- operations.py
+++ operations.py
@@ -10,3 +10,16 @@ def game_index(data):
         for developer in game.developer:
             print developer.name, ",",
         print game.release_date[:4], ")"
+
+def games_per_year(data):
+    year_list = []
+
+    for game in data:
+        if game.release_date is not None:
+            for year in year_list:
+                if game.release_date[:4] == year[0]:
+                    year = (year[0], year[1] + 1)
+            else:
+                year_list.append((game.release_date[:4], 1))
+
+    return year_list
