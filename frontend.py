aedace622f8a6adf232b186c5e87405e8d667563
eduardo <piscosour@gmail.com>
2013-06-16 01:27:36 -0500

    Add game now working, game shows up in list but is not stored beyond session.

--- frontend.py
+++ frontend.py
@@ -20,28 +20,43 @@ def hello_world():
 
 @app.route("/gamelist")
 def show_game_list():
-    gen_game_data = operations.gen_game_index()
+    global gamedata
+    gen_game_data = operations.gen_game_index(gamedata)
     return render_template("gamelist.html", game_data=gen_game_data)
 
 ## Individual game info render.
 
 @app.route("/game/<int:game_id>")
-def show_game(game_id, data=gamedata):
-    for game in data:
+def show_game(game_id):
+    global gamedata
+    for game in gamedata:
         if game_id == game.id:
             return render_template("game.html", selection=game)
     else:
-        return False
+        return "Game not found!"    
 
 ## Add game form
 
 @app.route("/addgame", methods=["GET", "POST"])
-def add_game(data=gamedata):
+def add_game():
+    global gamedata
     if request.method == "GET":
         return render_template("addgame.html")
     elif request.method == "POST":
-        data = data + [Game(request.form["title"], request.form["developer"], request.form["publisher"], request.form["release_date"], request.form["platforms"], request.form["technologies"], request.form["genre", request.form["dev_time"], request.form["distribution"], request.form["url"], request.form["category"])]
-        return render_template("add-game-confirmation.html", title=request.form["title"])
+        new_game = gameclasses.Game(id = len(gamedata) + 1, 
+                                    title = request.form["title"],
+                                    developer = request.form["developer"], 
+                                    publisher = request.form["publisher"], 
+                                    release_date = request.form["release_date"], 
+                                    platforms = request.form["platforms"], 
+                                    technologies = request.form["technologies"], 
+                                    genre = request.form["genre"], 
+                                    dev_time = request.form["dev_time"], 
+                                    distribution = request.form["distribution"], 
+                                    url = request.form["url"], 
+                                    game_category = request.form["category"])
+        gamedata = gamedata + [new_game]
+        return render_template("add-game-confirmation.html", game=new_game)
 
 
 if __name__ == "__main__":
